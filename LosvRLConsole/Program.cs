127bf046fb2bb745a24718d4cf13f15ef5d7d2d7
nrkn <nrkn.com@gmail.com>
2011-03-05 23:01:35 +1300

    first commit

---
+++ LosvRLConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LosvRLConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("LEGEND OF THE SOMETHING VILLAIN");
+      Console.WriteLine();
+      Console.WriteLine("Actually, I'm quite tired so will start tomorrow." );
+      Console.WriteLine("Press any key to disappointedly exit...");
+      Console.ReadKey();
+    }
+  }
+}
